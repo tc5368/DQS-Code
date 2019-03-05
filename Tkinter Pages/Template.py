8d3f978428f828c39c4af1d4ea6551b9fb34b7e1
Tom <c1769261@cardiff.ac.uk>
2019-03-05 14:33:29 +0000

    Changed to tab indentation

--- Tkinter Pages/Template.py
+++ Tkinter Pages/Template.py
@@ -7,23 +7,22 @@ class template(frame):
 		frame.__init__(self,master)
 		self.grid()
 		self.create_Buttons()
-        self.create_labels()
-        self.create_textboxes()
+		self.create_labels()
+		self.create_textboxes()
 
 	def create_Buttons():
 		Account_Options = Button(self, text='Account Options',command=self.account_options_fun)
-        Account_Options.grid(row=1,column=11)
-
+		Account_Options.grid(row=1,column=11)
 
 	def create_labels():
 		Title= StringVar()
-        Label(self,textvariable=Title).grid(row=1,column=7)
-        Title.set("Title")
+		Label(self,textvariable=Title).grid(row=1,column=7)
+		Title.set("Title")
 
 	def create_textboxes():
 		Question = StringVar()
-        Question = Entry(self, textvariable=Question)
-        Question.grid(row=0, column=1)
+		Question = Entry(self, textvariable=Question)
+		Question.grid(row=0, column=1)
 
 
 #Main
